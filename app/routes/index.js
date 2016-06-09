92396f4392d223d24dc65792a8dc3c3b2a16602d
Stefan Mortensen <stefan.mortensen@atomia.com>
2016-06-09 07:34:04 +0000

    Reformat javascript

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
-		database.query('SELECT * FROM servers LEFT JOIN roles ON roles.fk_server = servers.id', function (err, rows, field) {
-				if (err)
-						throw err;
-				res.render('index', { servers: rows });
-		});
+	database.query('SELECT * FROM servers LEFT JOIN roles ON roles.fk_server = servers.id', function (err, rows, field) {
+		if (err)
+			throw err;
+		res.render('index', { servers: rows });
+	});
 });
 module.exports = router;
